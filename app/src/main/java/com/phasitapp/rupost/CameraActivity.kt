c0f34bfda14df04542a409057fef126208953978
JT-FoxZone <jirayus.t63@rsu.ac.th>
2022-10-07 13:18:00 +0700

    Fix Camera Activity Null Lat Long

--- app/src/main/java/com/phasitapp/rupost/CameraActivity.kt
+++ app/src/main/java/com/phasitapp/rupost/CameraActivity.kt
@@ -43,8 +43,6 @@ import java.util.concurrent.Executors
 
 
 class CameraActivity : AppCompatActivity(), OnMapReadyCallback {
-    var latitude: String?= null
-    var longitude: String?= null
     var address_image: String? = null
     var name_image: String?= null
 
@@ -242,15 +240,16 @@ class CameraActivity : AppCompatActivity(), OnMapReadyCallback {
                 var bitmap = imageProxyToBitmap(image)
                 bitmap = rotateBitmap(bitmap, 90f)
 
-                var image_temp = viewToBitmap(findViewById(R.id.status_image))
-                var temp = viewToBitmap(findViewById(R.id.temp_text))
-                var status = viewToBitmap(findViewById(R.id.status_text))
+                //อย่าลืมลบ comment ก่อน commit (Note: Ford)
+                //var image_temp = viewToBitmap(findViewById(R.id.status_image))
+                //var temp = viewToBitmap(findViewById(R.id.temp_text))
+                //var status = viewToBitmap(findViewById(R.id.status_text))
 
-                image_temp = resizeBitmap(image_temp!!, 180)
-                temp = resizeBitmap(temp!!, 100)
-                status = resizeBitmap(status!!, 230)
+                //image_temp = resizeBitmap(image_temp!!, 180)
+                //temp = resizeBitmap(temp!!, 100)
+                //status = resizeBitmap(status!!, 230)
 
-                bitmap = combineImages(bitmap, image_temp, temp, status)!!
+                //bitmap = combineImages(bitmap, image_temp, temp, status)!!
 
                 val isSaveSuccessfully = savePhotoToInternalStorage(name, bitmap)
                 if (isSaveSuccessfully) {
@@ -336,8 +335,8 @@ class CameraActivity : AppCompatActivity(), OnMapReadyCallback {
                     Log.i(TAG, "onActivityResult CameraActivity: $imagePath")
 
                     ModelInfo = ModelPost(
-                        latitude = latitude,
-                        longitude = longitude,
+                        latitude = lat.toString(),
+                        longitude = long.toString(),
                         address = address_image,
                         images = arrayListOf(imagePath!!)
                     )
